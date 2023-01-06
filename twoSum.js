78dbd90bd956b56f2032bb1b548d06fc0d83919d
lechaim <lechaimz18@gmail.com>
2023-01-06 13:33:02 -0400

    Added comments to the twoSums problems

--- twoSum.js
+++ twoSum.js
@@ -4,14 +4,17 @@ You can return the answer in any order.*//
 
 
 var twoSum = function(nums, target) {
+    // Iterate through each array element and the second second element of the first loop (a+1)
     for (let a = 0; a < nums.length; a++){
         for (let b = a+1; b < nums.length; b++){
 
+            // if the sum of these numbers matches then return the positions of the elements
             if(nums[a] + nums[b] == target){
-                return [a,b]
+                return [a,b];
             }
         }
     }
 };
 
-console.log(twoSum([2,5,5,11], 10))
+console.log(twoSum([2,5,5,11], 10));
+console.log(twoSum([3,2,4], 6));
