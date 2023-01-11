5db500791030924b89271c8f752befcea10bcee6
lechaim <lechaimz18@gmail.com>
2023-01-11 12:27:22 -0400

    Finished the exercise and added a few comments

--- AddTwoNumbers.js
+++ AddTwoNumbers.js
@@ -1,12 +1,9 @@
-//You are given two non-empty linked lists representing two non-negative integers. 
-
-//The digits are stored in reverse order, and each of their nodes contains a single digit. 
-
-//Add the two numbers and return the sum as a linked list.
-
-//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-function listEnlarger(list, zeroAdder){
+/* You are given two non-empty linked lists representing two non-negative integers. 
+ The digits are stored in reverse order, and each of their nodes contains a single digit. 
+ Add the two numbers and return the sum as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself. */
+ 
+ function listEnlarger(list, zeroAdder){
     let addedZerosList = [];
 
     while(zeroAdder > 0){
@@ -16,18 +13,20 @@ function listEnlarger(list, zeroAdder){
 
     list.forEach(element => addedZerosList.push(element));
 
-    // hacer que devuelva una nueva lista con los 0's al principio
     return addedZerosList;
-}
+};
 
 var addTwoNumbers = function(l1, l2) {
 
     let listLenght = 0;
     let newList = [];
-    let counter = 0;
+    let counter = 0; 
     
+    // This conditional will be used when the list lenght are not tha same.
     if(l1.length != l2.length){
 
+        // With the variable zeroAdder the list listEnlarger function will add the diffence number of 0's to add
+        // overwritting the least size list to the size of the biggest one.  
         let zeroAdder = Math.abs(l1.length - l2.length);
 
         if(l1.length > l2.length){
@@ -38,8 +37,7 @@ var addTwoNumbers = function(l1, l2) {
         }
     }
 
-    //l1.length >= l2.length ? listLenght = l1.length : listLenght = l2.length; // find the number with biggest or equal number of elements 
-
+    // A loop to checkout all the items in the lists in backward.
     for(let i = l1.length - 1; i >= 0; i--){
 
         let number1 = l1[i];
@@ -48,24 +46,24 @@ var addTwoNumbers = function(l1, l2) {
         let adder = number1 + number2 + counter;
         counter = 0;  // to reset the used counter used in the previous number
 
+        // In case the sum of the numbers is greater or equal 10 the counter variable will be activated 
+        // to add the result to the next number
         if(adder >= 10) {
             adder -= 10;
             counter+= 1;
         }
-        //console.log(adder);
 
         newList.push(adder); // add the summed number to the new list
         
     }
+
     if (counter > 0){
-        newList.push(counter);
+        newList.push(counter); //This conditional will be used when the last summed number is greater or equal 10 and add the reamining value to the list
     }
 
     return (newList);
 };
 
 console.log(addTwoNumbers([2,4,3], [5,6,4])); // 708
-console.log(addTwoNumbers([0], [0]));
+console.log(addTwoNumbers([0], [0])); // [0]
 console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9])); // [8,9,9,9,0,0,0,1]
-
-//me lo esta tomando alreves por eso no va a funcionar, comienza a buscar en la segunda lista el 7mo item el cual no existe
